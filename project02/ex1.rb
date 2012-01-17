73770d160d9fe0f0da8ce07e89ea8f74accb2fd4
Andrew Suter-Morris <asutermo@mymail.mines.edu>
2012-01-16 21:17:25 -0700

    added project2 and example1 from learn ruby the hard way

---
+++ project02/ex1.rb
@@ -0,0 +1,11 @@
+#Andrew Suter-Morris - Windows
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts 'Yay! Printing.'
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+puts "\n"
+
+#puts "this is a comment"
