47a2a65ca853bb40cff3e5d848ba4b16e522f815
Andrew Suter-Morris <asutermo@mymail.mines.edu>
2012-01-24 00:27:29 -0700

    adding initial rails files

---
+++ project03/depot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'ec38ae506133278b2d9adfdfbea5cc7dc32acd989c3fadf00f6bdeae001dc39284ee55f822781a935502f534166bec721f8c4afa8ee01744b8cd57c7db7f5e8f'
