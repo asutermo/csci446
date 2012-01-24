f529553c0b93c6f167b1ba67ff28b99c21b8c890
Andrew Suter-Morris <asutermo@mymail.mines.edu>
2012-01-24 14:04:07 -0700

    trying again. rake is being a bitch

---
+++ project03/depot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'da2d48acdb7c7443e8bf017b236d29f80db1e9301ca53d79d2bf986239ad4082559763f9fc1dfe83dce15a8069d32768692e13964b97463356e8064e19b7bbd0'
