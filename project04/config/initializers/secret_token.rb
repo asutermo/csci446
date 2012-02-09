31ea9108980b7a23cecdcf54c9735e03ee818c78
Andrew <andrew@ubuntu.ubuntu-domain>
2012-02-09 11:45:03 -0700

    adding boilerplate files for Articl.es

---
+++ project04/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Articles::Application.config.secret_token = '85f05b91a4c5278797ebdc4fdadae2abd48ebce151630e71965e1b2c270921fc6594330ab08b759a4226f09a34adf5fd94d5b1fab83e8b50fa312909c91c6469'
