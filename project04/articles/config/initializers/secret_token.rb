618177e8637e726cd29877322ed566e173c8a18e
Andrew <andrew@ubuntu.ubuntu-domain>
2012-02-09 11:41:30 -0700

    redoing structure

--- project04/articles/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Articles::Application.config.secret_token = 'f702a60b4034dd3f40b218d1632f7b30250b16d94c0845eea53864bdb0a445cff684e9891ddb8c311fe19d6ceac03a973e65c4ac3576b8964acb4ba8632065b9'
