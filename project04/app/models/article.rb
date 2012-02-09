9ef92e1c3f1e9f2900768e8bed98283045f74143
Andrew Suter-Morris <asutermo@mymail.mines.edu>
2012-02-09 13:21:51 -0700

    added unit test for validating fields

--- project04/app/models/article.rb
+++ project04/app/models/article.rb
@@ -1,2 +1,3 @@
 class Article < ActiveRecord::Base
+	validates :title, :name, :body, presence: true
 end
--- project04/test/unit/article_test.rb
+++ project04/test/unit/article_test.rb
@@ -4,4 +4,16 @@ class ArticleTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def new_article()
+  	Article.new(title: "Test Article", name: "Test author", body: "woo",
+  												createdate: Time.now)
+  end
+  
+  test "article attributes must not be empty" do
+		article = Article.new
+  	assert article.invalid?
+  	assert article.errors[:title].any?
+  	assert article.errors[:name].any?
+  	assert article.errors[:body].any?
+  end
 end
