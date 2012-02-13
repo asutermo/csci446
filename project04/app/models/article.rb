9e04703ce7ba4d4d6d3ceeac0bd7fbd249732a61
Andrew Suter-Morris <asutermo@mymail.mines.edu>
2012-02-12 22:19:54 -0700

    added pat validation, modified some of the index.html for articles

--- project04/app/models/article.rb
+++ project04/app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ActiveRecord::Base
 	validates :title, :name, :body, presence: true
+	validates :name, format: {with:  %r{^((?!pat).*$)}i,
+		message: 'Must not have the name Pat'}
 end
