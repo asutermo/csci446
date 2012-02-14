613856257fd5a4e833677db64fca5a234b869d29
Andrew Suter-Morris <asutermo@mymail.mines.edu>
2012-02-14 13:35:21 -0700

    added mailer for email purposes

---
+++ project04/app/mailers/send_article.rb
@@ -0,0 +1,14 @@
+class SendArticle < ActionMailer::Base
+  default from: "from@example.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.send_article.send.subject
+  #
+  def send
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
---
+++ project04/db/migrate/20120214182539_create_articles.rb~
@@ -0,0 +1,13 @@
+class CreateArticles < ActiveRecord::Migration
+  def change
+    create_table :articles do |t|
+      t.string : title
+      t.string :author
+      t.text :body
+      t.datetime :create_date
+      t.integer :edit_count
+
+      t.timestamps
+    end
+  end
+end
