a3a559aadbf577f20a04fe03973b8f182dc29259
Andrew Suter-Morris <asutermo@mymail.mines.edu>
2012-02-14 11:39:09 -0700

    forgot to do migration, done now

--- project04/db/migrate/20120214182539_create_articles.rb
+++ project04/db/migrate/20120214182539_create_articles.rb
@@ -1,7 +1,7 @@
 class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
-      t.string : title
+      t.string :title
       t.string :author
       t.text :body
       t.datetime :create_date
