a02daeedb48ef30b5c46e5b1c93a468aec8c439f
Andrew Suter-Morris <asutermo@mymail.mines.edu>
2012-02-14 03:01:59 -0700

    Revert "migration for db. added edit column. started working on edit page"
    This reverts commit 7251ac08ebf26572079ff11e88bc356f7f9201d2.

--- project04/db/schema.rb
+++ project04/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120214034250) do
+ActiveRecord::Schema.define(:version => 20120209192345) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(:version => 20120214034250) do
     t.datetime "createdate"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.integer  "edit_count"
   end
 
 end
