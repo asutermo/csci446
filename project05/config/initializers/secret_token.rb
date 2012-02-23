e341c36ebe8d99b445514ebf332dce6d84128463
Andrew Suter-Morris <asutermo@mymail.mines.edu>
2012-02-23 11:53:05 -0700

    adding project 05 starting point

---
+++ project05/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Articles::Application.config.secret_token = '8ed0952f5e9be06397d47a9f8f377089ea9c4d2e5323bd65636857087b90bec51609e49ce6c8c320f55889e1e9a06695b041d529c1e3ddf8fa772d4bfcfc21d9'
