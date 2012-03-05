8cf2a54599665b36a40aaf07b7f9cd62fd1f66ef
Andrew Suter-Morris <asutermo@mymail.mines.edu>
2012-03-04 18:31:13 -0700

    seed data working. wtf rails

--- project05/app/models/article.rb
+++ project05/app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
 	validates :title, :body, :author_id, presence: true
-	belongs_to :author
+	belongs_to :author, :foreign_key => "author_id"
 end
--- project05/app/models/author.rb
+++ project05/app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ActiveRecord::Base
-	has_many :articles
+	has_many :articles, :dependent => :destroy
 	has_attached_file :photo
 	validates :authorname, :presence => true
 	validates :authorname, format: {with:  %r{^((?!pat).*$)}i, message: 'Must not have the name Pat'}
