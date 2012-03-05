96ecbb6db4acefc427d8e50d98bfb8d9d98fb644
Andrew Suter-Morris <asutermo@mymail.mines.edu>
2012-03-04 21:43:05 -0700

    added photo validation

--- project05/app/models/author.rb
+++ project05/app/models/author.rb
@@ -1,6 +1,7 @@
 class Author < ActiveRecord::Base
 	has_many :articles, :limit => 10, :dependent => :destroy
 	has_attached_file :photo
+	validates_attachment_presence :photo
 	validates :authorname, :presence => true
 	validates :authorname, format: {with:  %r{^((?!pat).*$)}i, message: 'Must not have the name Pat'}
 end
