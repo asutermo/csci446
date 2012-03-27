a8bd9170240a56e16abb58e10fb2987bd37bca7b
Andrew Suter-Morris <asutermo@mymail.mines.edu>
2012-03-26 20:41:55 -0600

    added validations to models

--- project06/app/models/game.rb
+++ project06/app/models/game.rb
@@ -1,2 +1,4 @@
 class Game < ActiveRecord::Base
+	validates :title, :rating, :user_id, presence: true
+	belongs_to :user, :foreign_key => "user_id"
 end
--- project06/app/models/user.rb
+++ project06/app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+	has_many :games, :limit => 10, :dependent => :destroy
+	validates :email, :user_name, :uniqueness => true
+	has_attached_file :photo, :styles => {:small => '200x200'}
+	validates_attachment_presence :photo
+	validates :user_name, :password, :f_name, :l_name, :email, presence: true
+	validates_attachment_content_type :photo, :content_type => ['image/png', 'image/jpeg']
 end
