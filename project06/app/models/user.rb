f2e86c2444fd319d786152e4db7dcf7b6dba8504
Andrew Suter-Morris <asutermo@mymail.mines.edu>
2012-03-29 12:49:13 -0600

    added namespace folders and email validation

--- project06/app/models/user.rb
+++ project06/app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 	acts_as_authentic
 	has_many :games, :limit => 10, :dependent => :destroy
 	validates :email, :username, :uniqueness => true
+	validates :email, :format => {:with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/, :on => :create}
 	has_attached_file :photo, :styles => {:small => '200x200'}
 	validates_attachment_presence :photo
 	validates :username, :password, :f_name, :l_name, :email, presence: true
