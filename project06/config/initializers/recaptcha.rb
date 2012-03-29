576381df36c0252074816dc4cec02db50fed0ce8
Andrew Suter-Morris <asutermo@mymail.mines.edu>
2012-03-28 21:58:44 -0600

    recpatcha added to registration page

---
+++ project06/config/initializers/recaptcha.rb
@@ -0,0 +1,5 @@
+Recaptcha.configure do |config|
+  config.public_key  = '6Lcfi88SAAAAAFVQnk4YecXn0AjZJvwUEiqO_0xr'
+  config.private_key = '6Lcfi88SAAAAACVV_iWRrRR9rP4aNlXtLE9pbxzox'
+  config.proxy = 'http://myrpoxy.com.au:8080'
+end
