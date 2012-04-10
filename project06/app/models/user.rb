6a52bb5845f9a83c98a00ef402e4af0767978b92
Andrew Suter-Morris <asutermo@mymail.mines.edu>
2012-04-09 23:31:06 -0600

    fixed users model

--- project06/app/models/user.rb
+++ project06/app/models/user.rb
@@ -1,13 +1,16 @@
 class User < ActiveRecord::Base
 	acts_as_authentic
-	has_many :games
-	belongs_to :role
-	has_attached_file :photo
+  belongs_to :role
+  has_attached_file :photo
+	has_many :games, :dependent => :destroy
+	validates :email, :username, :uniqueness => true
+  validates :email, :format => {:with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/, :on => :create}
 	validates :f_name, :l_name, :presence => true
   validates :password, :confirmation => true, :presence => true, :on => :create
   validates_length_of :password, :minimum => 6, :on => :create
 	validates_length_of :username, :minimum => 6
-
+  has_attached_file :photo, :styles => {:small => '200x200'}
+  validates_attachment_presence :photo
   def full_name
     self.f_name.capitalize + " " + self.l_name.capitalize
   end
