b6689372575cfbb0558fe4415d23f2ce9f73934a
Andrew Suter-Morris <asutermo@mymail.mines.edu>
2012-04-15 18:43:07 -0600

    random numbers working now. added field to html for testing

--- project07/main.js
+++ project07/main.js
@@ -22,5 +22,7 @@ function guess() {
 }
 
 function getRandom() {
-	return Math.floor(Math.random() * 101)
+	var rand = Math.floor(Math.random() * 101);
+	$('h2#number span#randNum').append(rand);
+	return rand;
 }
